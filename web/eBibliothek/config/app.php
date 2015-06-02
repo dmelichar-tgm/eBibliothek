1c92fbf3bc8cfb977fd6124c0211bbffd2ddcc41
TGMdkocsi <kocsidaniel@gmail.com>
2015-06-02 11:03:33 +0200

    erweiterung provider

--- web/eBibliothek/config/app.php
+++ web/eBibliothek/config/app.php
@@ -135,6 +135,7 @@ return [
 		'Illuminate\Translation\TranslationServiceProvider',
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
+		'Bican\Roles\RolesServiceProvider',
 
 		/*
 		 * Application Service Providers...
