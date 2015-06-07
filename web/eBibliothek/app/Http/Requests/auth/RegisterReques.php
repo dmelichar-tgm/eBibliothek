57deea6cd566236f28f523ec57071afdd42170df
Fitim Faiku <fitim_faiki@hotmail.com>
2015-06-07 05:00:11 +0200

    registeranfrage

---
+++ web/eBibliothek/app/Http/Requests/auth/RegisterReques.php
@@ -0,0 +1,34 @@
+<?php namespace todoaparrot\Http\Requests\Auth;
+
+use todoaparrot\Http\Requests\Request;
+
+class RegisterReques extends Request {
+
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		return true;
+	}
+
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+		        return [
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed|min:8',
+        ];
+	}
+	public function authorize()
+    {
+        return true;
+    }
+
+}
