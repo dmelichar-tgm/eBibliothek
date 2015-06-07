6fe1ffde5cf9f1b20c78dfdca2e8e90bb18e9b83
Fitim Faiku <fitim_faiki@hotmail.com>
2015-06-07 04:56:01 +0200

    Auth Controller

--- web/eBibliothek/app/Http/Controllers/Auth/AuthController.php
+++ web/eBibliothek/app/Http/Controllers/Auth/AuthController.php
@@ -6,79 +6,96 @@ use eBibliothek\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller {
-
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		//
-	}
-
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
-
+ 
+    /**
+     * the model instance
+     * @var User
+     */
+    protected $user; 
+    /**
+     * The Guard implementation.
+     *
+     * @var Authenticator
+     */
+    protected $auth;
+ 
+    /**
+     * Create a new authentication controller instance.
+     *
+     * @param  Authenticator  $auth
+     * @return void
+     */
+    public function __construct(Guard $auth, User $user)
+    {
+        $this->user = $user; 
+        $this->auth = $auth;
+ 
+        $this->middleware('guest', ['except' => ['getLogout']]); 
+    }
+ 
+    /**
+     * Show the application registration form.
+     *
+     * @return Response
+     */
+    public function getRegister()
+    {
+        return view('auth.register');
+    }
+ 
+    /**
+     * Handle a registration request for the application.
+     *
+     * @param  RegisterRequest  $request
+     * @return Response
+     */
+    public function postRegister(RegisterRequest $request)
+    {
+        $this->user->email = $request->email;
+    	$this->user->password = bcrypt($request->password);
+    	$this->user->save();
+        $this->auth->login($this->user); 
+        return redirect('/dash-board'); 
+    }
+ 
+    /**
+     * Show the application login form.
+     *
+     * @return Response
+     */
+    public function getLogin()
+    {
+        return view('auth.login');
+    }
+ 
+    /**
+     * Handle a login request to the application.
+     *
+     * @param  LoginRequest  $request
+     * @return Response
+     */
+    public function postLogin(LoginRequest $request)
+    {
+        if ($this->auth->attempt($request->only('email', 'password')))
+        {
+            return redirect('/dash-board');
+        }
+ 
+        return redirect('/login')->withErrors([
+            'email' => 'The credentials you entered did not match our records. Try again?',
+        ]);
+    }
+ 
+    /**
+     * Log the user out of the application.
+     *
+     * @return Response
+     */
+    public function getLogout()
+    {
+        $this->auth->logout();
+ 
+        return redirect('/');
+    }
+ 
 }
